ef157674d1ae5432726777341f803b016b6b4513
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-07-20 21:04:11 +0530

    Major Changes. Website pagination introduced; Logo issues fixed; Stories page UI almost ready.

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ This is the official Homepage of the [Open Source Development Group](http://osdg
 
 ### TODOS
 
-* Basic UI Design.
-
 * A section for inspirational blogs on the philosophy of Open Source Development and [articles on how students started working on their respective orgs.] (https://docs.google.com/document/d/1yeaIctCq7qLnE7TPj0P8G1-zgX3yo0TBhB8YZIFZha0/edit)
 
 * Tutorials on what is Version Control/Git and how to use the IRC.
---
+++ contributors.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+	<title>OSDG : IIIT-Hyderabad</title>
+	<link rel="stylesheet" href="./css/standard.css" />
+</head>
+<body>
+<?php include_once('header.php'); ?>
+
+<div id="container">
+
+	<h1>Contributors to this website</h1>
+<p><a href="http://mrinaldhar.com">Mrinal Dhar</a>: Website Design</p>
+
+<p><a href="https://www.linkedin.com/in/adhishsingla">Adhish Singla</a>: The OSDG logo</p>
+
+<p><a href="http://vatikaharlalka.wordpress.com">Vatika Harlalka</a>: Static page content</p>
+
+<p><a href="http://amoghbl1.me">Amogh Pradeep</a>: This guy we keep around for merging pull requests... and being OSDG admin.</p>
+<br />
+<br />
+<br />
+<p>Join the list! Start now by forking <a href="https://github.com/OSDG-IIITH/www">this repository</a>.</p>
+
+</div>
+<?php include_once('footer.php'); ?>
+</body>
+<script src="./js/jquery.js"></script>
+<script src="./js/jqueryui.js"></script>
+<script src="./js/standard.js"></script>
+<script>
+
+</script>
+</html>
