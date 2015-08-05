3061f4a2f19fe890bade545cc82800a945ced67b
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2015-08-05 15:09:09 +0530

    Cleaned up indentation and spacing of js and css

--- js/standard.js
+++ js/standard.js
@@ -1,23 +1,18 @@
+var osdgColors = ['#1C75BC', '#ED1C24','#FFF200', '#00A14B'];
 
-	
-	var osdgColors = ['#1C75BC', '#ED1C24','#FFF200', '#00A14B'];
+$('#nav li').mouseover(function() {
+	var randomColor = osdgColors[parseInt(Math.random() * 10000) % 4];
+	// console.log(randomColor);
+	$(this).css({"background-color": randomColor});
+	if (randomColor == '#FFF200') {
+		$(this).css({"color": "#000000"});
+	}
+	else {
+		$(this).css({"color": "#ffffff"});
+	}
+});
 
-	$('#nav li').mouseover(function() {
-		var randomColor = osdgColors[parseInt(Math.random()*10000)%4];
-			// console.log(randomColor);
-		$(this).css({"background-color":randomColor});
-		if (randomColor == '#FFF200') {
-		$(this).css({"color":"#000000"});
-
-		}
-		else {
-		$(this).css({"color":"#ffffff"});
-
-		}
-	});
-	$('#nav li').mouseout(function() {
-		$(this).css({"background-color":""});
-		$(this).css({"color":""});
-	});
-
-	
+$('#nav li').mouseout(function() {
+	$(this).css({"background-color": ""});
+	$(this).css({"color": ""});
+});
--- js/stories.js
+++ js/stories.js
@@ -1,66 +1,63 @@
 // var stories = 0;
-	var stories;
+var stories;
 load_stories();
-	function load_stories() {
-
-			$.ajax({
- 	 type: "GET",
- 	 url: "stories/stories.json",
- 	 async: false,
-	success:function(response){
-                    stories = response;
-                },
-  });
-		
-			stories["stories"].sort(function(a, b) {
-   			return a["author"].localeCompare(b["author"]);
-			});
-			render_list();
-
-
-			
-
-	}
-
-	function render_list() {
-		$('#stories_list').find('.storyitem').remove();
-		$.each(stories["stories"], function(index, value) {
-				var item = "";
-				item += "<li class='storyitem' onclick='getStory("+index+")'>"+value["author"]+"<small>"+value["org"]+"</small><br /><span class='tags'>";
-				$.each(stories["stories"][index]["tags"].split(","), function (num, val) {
-				item += val+" &bull; ";
-				});
-			item += "</span></li>";
-				$('#stories_list').append(item);
-				console.log();
-			});
-		$('#sort_selector tr td').click(function() {
-			// alert('hi');
-		 $(this).addClass('selected').siblings().removeClass('selected');
+function load_stories() {
+	$.ajax({
+		type: "GET",
+		url: "stories/stories.json",
+		async: false,
+		success: function(response) {
+			stories = response;
+		}
 	});
-		$('#stories_list li').not('.control_item').click(function() {
-			// alert('hi');
-		 $(this).addClass('selected_story').siblings().removeClass('selected_story');
-	});
-	}
-	function sortby(value) {
-		stories["stories"].sort(function(a, b) {
-   			return a[value].localeCompare(b[value]);
-			});
-		render_list();
-	}
-	function getStory(index) {
-		$('#stories_content').fadeOut("fast", function() {
-			$.ajax({
- 	 type: "GET",
- 	 url: "stories/"+stories["stories"][index]["filename"],
- 	 async: false,
-	success:function(response){
-		$('#stories_content').text(response);
-		$('#stories_content').fadeIn();
 
-                },
-  });
+	stories["stories"].sort(function(a, b) {
+		return a["author"].localeCompare(b["author"]);
+	});
+	render_list();
+}
+
+function render_list() {
+	$('#stories_list').find('.storyitem').remove();
+	$.each(stories["stories"], function(index, value) {
+		var item = "";
+		item += "<li class='storyitem' onclick='getStory("+index+")'>"+value["author"]+"<small>"+value["org"]+"</small><br /><span class='tags'>";
+		$.each(stories["stories"][index]["tags"].split(","), function (num, val) {
+			item += val+" &bull; ";
 		});
-		
-	}
+		item += "</span></li>";
+		$('#stories_list').append(item);
+		console.log();
+	});
+
+	$('#sort_selector tr td').click(function() {
+		// alert('hi');
+		$(this).addClass('selected').siblings().removeClass('selected');
+	});
+
+	$('#stories_list li').not('.control_item').click(function() {
+		// alert('hi');
+		$(this).addClass('selected_story').siblings().removeClass('selected_story');
+	});
+}
+
+function sortby(value) {
+	stories["stories"].sort(function(a, b) {
+		return a[value].localeCompare(b[value]);
+	});
+	render_list();
+}
+
+function getStory(index) {
+	$('#stories_content').fadeOut("fast", function() {
+		$.ajax({
+			type: "GET",
+			url: "stories/"+stories["stories"][index]["filename"],
+			async: false,
+			success: function(response) {
+				$('#stories_content').text(response);
+				$('#stories_content').fadeIn();
+			}
+		});
+	});
+}
