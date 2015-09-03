c86da854d39212a3a767d44f5b52e77f9eeeadc9
Sriharsh Bhyravajjula <hbhyrava@gmail.com>
2015-09-03 13:46:27 +0530

    A few edits to the introductory text.
    Grammatical errors have been corrected.

--- intro.php
+++ intro.php
@@ -1,12 +1,12 @@
 
 <p>Remember how when we were kids, we’d always create a fuss to have our way?
-Mom’s chocolate cookies had to have the chocolate sprinkles, the jacket you’re
-getting must be the one you want, no matter what the fashion nazis said, and
-Sundays always meant it’s time to visit your cousin. <br /><br />
+Mom’s chocolate cookies had to have the chocolate sprinkles, the jacket you were
+getting had to be the one you wanted no matter what the fashion nazis said, and
+Sundays always meant it was time to visit your cousin. <br /><br />
 Proprietary software is
 like the the chocolate you buy at a store, which comes wrapped up, and all you
 can do is taste it and then, recommend it to your friends or distribute it to
-your class on your birthday if you like it. Under far-fetched situations, you
+your class on your birthday if you liked it. Under far-fetched circumstances, you
 may try to reverse-engineer it. <br /><br />Open Source Software, in contrast, is like the
 scrumptious chocolate batter you come up with when your friends are home and
 you decide to make something awesome! When Mom isn’t around, you enter the
@@ -15,8 +15,8 @@ ingredients, mix them in a bowl, take a deep breath and give it a taste. Yuck!
 Tastes awful doesn’t it? What do you do? You call your friends to help! But
 Mom can’t have mess in the kitchen, so you’ve got to have some order right? So
 you distribute the batter among your friends and let each one experiment and
-taste what they have. You can always merge the best ones and have the most
-delicious batter right? Could you have ever made this batter alone?
+taste what they have made. You can always merge the best ones and have the most
+delicious batter, right? Could you have ever made this batter all by yourself?
 </p>
 
 <p>
@@ -30,11 +30,11 @@ where they’d ask you what you’re doing and how to get started on it, and you
 redirect them to a `Repository` having whatever you’ve done so far and give
 them advice on how to continue from there. <br /><br />Once they’ve made changes on their
 branch, they would send you a `pull request` which you could then `merge` into
-your `Repo` if you feel you like it. Before you know it, you have the best
+your `Repo` if you think you like it. Before you know it, you have the best
 collaborative effort!
 </p>
 <p>
 OpenVPN, Mozilla, Xchat, OpenStack, Tor are just a few of the thousands of
-orgs that have gone the Open Source way :) We hope to find you on the IRC’s
+orgs that have gone the Open Source way :) We hope to find you on the IRCs
 soon!
 </p>
