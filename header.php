430fb725942492aed04a2c8a79a88b361810b356
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-11-11 13:26:20 +0530

    Minor change

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
 	<td width="90%" style="padding: 0px;">
 <ul id="nav">
 	<a href="index.php"><li id="intro_li" class="item anim">Intro to Open Source</li></a>
-	<a href="construction.php"><li id="blog_li" class="item anim">OSDG Blog</li>
-	<a href="construction.php"><li id="ranker_li" class="item anim">Github Ranker</li>
+	<a href="construction.php"><li id="blog_li" class="item anim">OSDG Blog</li></a>
+	<a href="construction.php"><li id="ranker_li" class="item anim">Github Ranker</li></a>
 	<a href="stories.php"><li id="stories_li" class="item anim">Member stories</li></a>
-	<a href="construction.php"><li id="calender_li" class="item anim">Calendar</li>
+	<a href="construction.php"><li id="calender_li" class="item anim">Calendar</li></a>
 </ul>
 </td>
 </tr>
